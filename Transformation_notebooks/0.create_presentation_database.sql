d75116c7a21c02ce9fce8528b4a266f6ca86722b
Prajwal_Gedam <91496751+Prajwal010@users.noreply.github.com>
2023-09-10 18:22:48 +0530

    Add files via upload

---
+++ Transformation_notebooks/0.create_presentation_database.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS f1_presentation
+LOCATION "/mnt/formula1projectpg/presentation"
+
+-- COMMAND ----------
+
+Desc database f1_presentation
+
+-- COMMAND ----------
+
