d76559cd5e2ff3c94dd859c864077191c6fab73b
Prajwal_Gedam <91496751+Prajwal010@users.noreply.github.com>
2023-09-10 18:23:54 +0530

    Add files via upload

---
+++ Includes/common_functions.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+# Function to get current timestamp col
+
+from pyspark.sql.functions import current_timestamp
+def add_ingestion_date(input_df):
+    output_df = input_df.withColumn("ingestion_date",current_timestamp()) 
+    return output_df
+
+# COMMAND ----------
+
+# Function to get col names in a list
+
+def df_column_to_list(input_df, column_name):
+  df_row_list = input_df.select(column_name) \
+                        .distinct() \
+                        .collect()
+  
+  column_value_list = [row[column_name] for row in df_row_list]
+  return column_value_list
+
+# COMMAND ----------
+
+# Function to put partition By column at the end of the list
+
+def re_arrange_partition_column(input_df, partition_column):
+  column_list = []
+  for column_name in input_df.schema.names:
+    if column_name != partition_column:
+      column_list.append(column_name)
+  column_list.append(partition_column)
+  output_df = input_df.select(column_list)
+  return output_df
+
+# COMMAND ----------
+
+# Function to overwrite partition (avoids duplicate insertion) for DataLake
+
+def overwrite_partition(input_df, db_name, table_name, partition_column):
+  output_df = re_arrange_partition_column(input_df, partition_column)
+  spark.conf.set("spark.sql.sources.partitionOverwriteMode","dynamic")
+  if (spark._jsparkSession.catalog().tableExists(f"{db_name}.{table_name}")):
+    output_df.write.mode("overwrite").insertInto(f"{db_name}.{table_name}")
+  else:
+    output_df.write.mode("overwrite").partitionBy(partition_column).format("parquet").saveAsTable(f"{db_name}.{table_name}")
+
+# COMMAND ----------
+
+# Function to merge incremental data and overwrite the new data for Delta lake
+
+def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition, partition_column):
+  spark.conf.set("spark.databricks.optimizer.dynamicPartitionPruning","true")
+
+  from delta.tables import DeltaTable
+  if (spark._jsparkSession.catalog().tableExists(f"{db_name}.{table_name}")):
+    deltaTable = DeltaTable.forPath(spark, f"{folder_path}/{table_name}")
+    deltaTable.alias("tgt").merge(
+        input_df.alias("src"),
+        merge_condition) \
+      .whenMatchedUpdateAll()\
+      .whenNotMatchedInsertAll()\
+      .execute()
+  else:
+    input_df.write.mode("overwrite").partitionBy(partition_column).format("delta").saveAsTable(f"{db_name}.{table_name}")
---
+++ Includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = '/mnt/formula1projectpg/raw'
+processed_folder_path = '/mnt/formula1projectpg/processed'
+presentation_folder_path = '/mnt/formula1projectpg/presentation'
